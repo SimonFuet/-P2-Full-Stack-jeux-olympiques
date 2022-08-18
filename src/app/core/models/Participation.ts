08a485ef50427255cf6ac6893d6a11e119d74fbe
Yannick <yannick.mougin21@gmail.com>
2022-08-18 14:31:50 +0200

    create starter code

---
+++ src/app/core/models/Participation.ts
@@ -0,0 +1,11 @@
+// TODO: create here a typescript interface for a participation
+/*
+example of participation:
+{
+    id: 1,
+    year: 2012,
+    city: "Londres",
+    medals_count: 28,
+    athlete_count: 372
+}
+*/
